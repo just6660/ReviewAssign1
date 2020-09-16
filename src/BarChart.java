5a91ab24897ecee7254e655cdf459dfdf6c1b820
just6660 <just6660@ISS2TZQKH2.scdsb.pri>
2020-09-16 08:34:59 -0400

    Finished barchart

--- src/BarChart.java
+++ src/BarChart.java
@@ -12,7 +12,7 @@ public class BarChart {
         
         Scanner s = new Scanner(System.in);
         
-        int value1, value2, value3, value4, highest = 0; 
+        int value1, value2, value3, value4, highest = 0, lowest = 0; 
         
         System.out.print("Enter value 1 of 4 > ");
         value1 = s.nextInt();
@@ -26,22 +26,39 @@ public class BarChart {
         System.out.print("Enter value 4 of 4 > ");
         value4 = s.nextInt();
         
-        if (highest > 400){
-            System.out.println("Error - number must be less than 400");
+        
+        if(value1 >= value2 && value1 >= value3 && value1 >= value4)
+            highest = value1;
+        
+        else if(value2 >= value1 && value2 >= value3 && value2 >= value4)
+            highest = value2;
+        
+        else if(value3 >= value1 && value3 >= value2 && value3 >= value4)
+            highest = value3;
+        
+        else
+            highest = value4;
+        
+        if(value1 <= value2 && value1 <= value3 && value1 <= value4)
+            lowest = value1;
+        
+        else if(value2 <= value1 && value2 <= value3 && value2 <= value4)
+            lowest = value2;
+        
+        else if(value3 <= value1 && value3 <= value2 && value3 <= value4)
+            lowest = value3;
+        
+        else
+            lowest = value4;
+        
+        if(highest>400 || lowest <50){
+            System.out.println("Error - number must be bewteen 50 and 400");
             return;
         }
         
-        if(value1 >= value2)
-            highest = value1;
-        if(value2 > value1)
-            highest = value2;
-        if (highest < value3)
-            highest = value3;
-        if(highest < value4)
-            highest = value4;
-
         
-        win.setSize(500, highest * 2 + 200);
+ 
+        win.setSize(500, highest * 2);
                 
         
         p.setColor(Color.black);
